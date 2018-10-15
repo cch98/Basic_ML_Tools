8ead701d467058827d899c88039780e5bd598f66
cch98 <changho9808@gmail.com>
2018-10-15 21:55:29 +0900

    air data preprocessing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 from sklearn import tree
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
@@ -16,15 +17,20 @@ car_data.pop() #eliminate the last element
 
 data = open("AirQualityUCI.csv", 'r')
 airdata = data.read()
+
+airdata = airdata.replace(',', '.')
 airdata = airdata.split()
 
 air_data = []
 for i in airdata:
-    j = i.split(",")
+    j = i.split(";")
+    j.pop()
+    j.pop()
     air_data.append(j)
 
-air_data.pop()
-#print(air_data)
+# print(air_data)
+# print(len(air_data))
+# print(len(air_data[0]))
 
 
 #-----------------accuray fucntion-----------#
@@ -63,7 +69,7 @@ def accuracy_func(tag, result):
 
 
 # program object:
-# guess the buying(price) of the car with other attributes
+# guess the class with attributes
 
 
 #==================preprocessing====================
@@ -192,3 +198,85 @@ mlp_acc = accuracy_func(Ycar_data, result)
 print("Accuracy of MLP: " + str(mlp_acc))
 
 #================Logistic Regression=============
+
+
+
+
+
+#----------------Air Quality data----------------#
+
+# Attribute Information:
+#
+# 0 Date	(DD/MM/YYYY)
+# 1 Time	(HH.MM.SS)
+# 2 True hourly averaged concentration CO in mg/m^3 (reference analyzer)
+# 3 PT08.S1 (tin oxide) hourly averaged sensor response (nominally CO targeted)
+# 4 True hourly averaged overall Non Metanic HydroCarbons concentration in microg/m^3 (reference analyzer)
+# 5 True hourly averaged Benzene concentration in microg/m^3 (reference analyzer)
+# 6 PT08.S2 (titania) hourly averaged sensor response (nominally NMHC targeted)
+# 7 True hourly averaged NOx concentration in ppb (reference analyzer)
+# 8 PT08.S3 (tungsten oxide) hourly averaged sensor response (nominally NOx targeted)
+# 9 True hourly averaged NO2 concentration in microg/m^3 (reference analyzer)
+# 10 PT08.S4 (tungsten oxide) hourly averaged sensor response (nominally NO2 targeted)
+# 11 PT08.S5 (indium oxide) hourly averaged sensor response (nominally O3 targeted)
+# 12 Temperature in Â°C
+# 13 Relative Humidity (%)
+# 14 AH Absolute Humidity
+
+#program object
+#guess the Absolute Humidity with other attributes
+
+#====================preprocessing======================
+
+Xair_data = []
+Yair_data = []
+
+# cMax = []
+# cMin = []
+# for i in range(len(air_data[0])-1):
+#     cMax.append(-1e10)
+#     cMin.append(1e10)
+
+for i in air_data:
+    t = []
+
+    date = i[0]
+    time = i[1]
+    dt = date+' '+time
+    d = datetime.strptime(dt, '%d/%m/%Y %H.%M.%S')
+    t.append(d.timestamp())
+
+    # if(cMax[0]<t[0]):
+    #     cMax[0] = t[0]
+    # if(cMin[0]>t[0]):
+    #     cMin[0] = t[0]
+
+    for j in range(len(i)-3):
+        t.append(float(i[j+2]))
+
+        # if(t[j+1] > -200):
+        #     if (cMax[j+1] < t[j+1]):
+        #         cMax[j+1] = t[j+1]
+        #     if (cMin[j+1] > t[j+1]):
+        #         cMin[j+1] = t[j+1]
+
+    Xair_data.append(t)
+
+    k = float(i[14])
+    if (k <= -200):
+        k = -1
+    else:
+        k *= 2
+        k = int(k)
+    Yair_data.append(k)
+
+# print(Xair_data)
+print(Yair_data)
+
+# print(cMax)
+# print(cMin)
+# [1112590800.0, 11.9, 2040.0, 1189.0, 63.7, 2214.0, 1479.0, 2683.0, 340.0, 2775.0, 2523.0, 44.6, 88.7, 2.231]
+# [1078909200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0, -200.0]
+# [1112590800.0, 11.9, 2040.0, 1189.0, 63.7, 2214.0, 1479.0, 2683.0, 340.0, 2775.0, 2523.0, 44.6, 88.7, 2.231]
+# [1078909200.0, 0.1, 647.0, 7.0, 0.1, 383.0, 2.0, 322.0, 2.0, 551.0, 221.0, -1.9, 9.2, 0.1847]
+
